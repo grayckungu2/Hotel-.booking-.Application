c086610113b12f83547c568cd25e08f8c2b5e467
Grace kungu <grayckungu2@gmail.com>
2023-07-04 09:24:04 +0300

    update changes

---
+++ js/index.js
@@ -0,0 +1,8 @@
+const url = 'https://booking-com.p.rapidapi.com/v2/hotels/details?hotel_id=1676161&currency=AED&locale=en-gb&checkout_date=2023-09-28&checkin_date=2023-09-27';
+const options = {
+	method: 'GET',
+	headers: {
+		'X-RapidAPI-Key': '1deea4c09amsh3f251fe6f82b9f3p1d354bjsne182995ce605',
+		'X-RapidAPI-Host': 'booking-com.p.rapidapi.com'
+	}
+};
