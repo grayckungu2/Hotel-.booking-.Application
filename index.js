a4eccd4a3e64f22b2542dfff96fa3f5750ed9990
Grace kungu <grayckungu2@gmail.com>
2023-07-05 09:43:04 +0300

    update

--- index.js
+++ index.js
@@ -1,8 +1,74 @@
-const url = 'https://booking-com.p.rapidapi.com/v2/hotels/details?hotel_id=1676161&currency=AED&locale=en-gb&checkout_date=2023-09-28&checkin_date=2023-09-27';
-const options = {
-	method: 'GET',
-	headers: {
-		'X-RapidAPI-Key': '1deea4c09amsh3f251fe6f82b9f3p1d354bjsne182995ce605',
-		'X-RapidAPI-Host': 'booking-com.p.rapidapi.com'
-	}
-};
+// code solution
+
+const URL = "http://localhost:3000/Hotelpaymentfeatures";
+
+let listholder = document.getElementById("Hotelpaymentfeatures");
+document.addEventListener('DOMContentLoaded', () => {
+  document.getElementsByClassName("paymentfeatures")[0].remove();
+  fetchPaymentFeatures(URL);
+});
+
+function fetchPaymentFeatures(URL) {
+}
+
+function fetchPaymentFeatures(URL) {
+    fetch(URL)
+      .then(response => response.json())
+      .then(paymentFeatures => {
+        paymentFeatures.forEach(paymentFeature => {
+          displayPaymentFeature(paymentFeature);
+        });
+      });
+  }
+  function displayPaymentFeature(paymentFeature) {
+    const li = document.createElement('li');
+    li.style.cursor = "pointer";
+    li.textContent = paymentFeature.toUpperCase();
+    listHolder.appendChild(li);
+    addClickEvent(li);
+  }
+  
+  function addClickEvent(element) {
+    element.addEventListener('click', () => {
+      fetch(`${url}/${element.textContent.toLowerCase()}`)
+        .then(res => res.json())
+        .then(paymentFeature => {
+          document.getElementById('book').textContent = 'book';
+          setUpPaymentFeature(paymentFeature);
+        });
+    });
+  }
+
+  function displayPaymentFeatures() {
+    const paymentFeatures = data.Hotelpaymentfeatures;
+    const paymentFeaturesContainer = document.getElementById("paymentFeaturesContainer"); // Assuming there is an element with id "paymentFeaturesContainer" in your HTML
+  
+    paymentFeatures.forEach(feature => {
+      const featureElement = document.createElement("div");
+      featureElement.innerHTML = `
+        <p>Bookable: ${feature.bookable}</p>
+        <p>CVC Required: ${feature.cvc_required}</p>
+        <p>Payable: ${feature.payable}</p>
+        <p>Is Direct Payment: ${feature.is_direct_payment}</p>
+        <p>Credit Card ID: ${feature.creditcard_id}</p>
+        <p>Hotel ID: ${feature.hotel_id}</p>
+        <button class="bookingButton">Make Booking</button>
+      `;
+      paymentFeaturesContainer.appendChild(featureElement);
+    });
+  
+    const bookingButtons = document.getElementsByClassName("bookingButton");
+    for (let i = 0; i < bookingButtons.length; i++) {
+      bookingButtons[i].addEventListener("click", function() {
+        makeBooking(paymentFeatures[i].bookable);
+      });
+    }
+  }
+  
+  function makeBooking(bookable) {
+    if (bookable) {
+      alert("Booking successful!");
+    } else {
+      alert("Sorry, this feature is not bookable.");
+    }
+  }
